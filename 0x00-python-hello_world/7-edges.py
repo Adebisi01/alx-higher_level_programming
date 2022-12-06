dba4bd4f91a5d44beadeb4566c3d5d9612328bec
Adebisi <oluwafemiadebisi01@gmail.com>
2022-12-06 21:06:02 +0100

    words

--- 0x00-python-hello_world/7-edges.py
+++ 0x00-python-hello_world/7-edges.py
@@ -1,8 +1,7 @@
-UR CODE GOES HERE. PLEASE REMOVE THIS LINE
-print(f"First 3 letters: {word_first_3}")
-print(f"Last 2 letters: {word_last_2}")
-print(f"Middle word: {middle_word}")
-/bin/bash: word: command not found
+#!/usr/bin/python3
+word ='Holberton'
+word_first_3 = word[:3]
+word_last_2 = word[-2:]
 middle_word = word[1:-1]
 print(f"First 3 letters: {word_first_3}")
 print(f"Last 2 letters: {word_last_2}")
