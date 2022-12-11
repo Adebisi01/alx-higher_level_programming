797c6ec60474bcf6415fd70c938cf37438b32475
Adebisi <oluwafemiadebisi01@gmail.com>
2022-12-11 13:22:51 +0100

    upper

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,7 +2,7 @@
 def uppercase(charac):
     for i in range(0, len(charac) + 1):
         if ord(charac[i]) >= 97 and ord(charac[i]) <= 122:
-            char = ord(charac[i]) + 32
+            char = ord(charac[i]) - 32
         else:
             char = ord(charac[i])
         print("{}".format(chr(char)), end='')
