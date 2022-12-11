79700f6d7aff16b6bf7235ea3324a52d27786c21
Adebisi <oluwafemiadebisi01@gmail.com>
2022-12-11 14:35:41 +0100

    remove

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -3,7 +3,7 @@ def remove_char_at(str, n):
     string = str
     if n < 0:
         return string
-    elif len(string) >= n:
+    elif len(string) <= n:
         return string
     else:
         newString = string.replace(string[n], '')
